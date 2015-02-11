65a8a1fc6eec04182a9a34379f920ed44e735de5
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-02-11 14:51:09 +0100

    asking the user to stop jxcore monitor before extansion update

---
+++ JXcore_Plesk/plib/scripts/pre-install.php
@@ -0,0 +1,21 @@
+<?php
+
+pm_Context::init("jxcore-support");
+
+if (false !== ($upgrade = array_search('upgrade', $argv))) {
+
+    $previous_version = $argv[2];
+
+    if ($previous_version < '0.2.5') {
+        $urlMonitor = "https://localhost:17777/json?silent=true";
+        $json = null;
+        $monitorRunning = Modules_JxcoreSupport_Common::getURL($urlMonitor, $json);
+
+        if ($monitorRunning) {
+            print("Please stop JXcore monitor before updating the extension.");
+            exit(1);
+        }
+    }
+}
+
+exit(0);
