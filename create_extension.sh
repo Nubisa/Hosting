60aa6c093ecdba84166e0269b86fd9e7fbc32de3
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-06-03 13:57:31 +0200

    alternative for jx compile

--- create_extension.sh
+++ create_extension.sh
@@ -2,7 +2,9 @@
 rm JXcore_Plesk.zip
 cd JS
 jx compile service.jxp
-jx compile spawner.jxp
+jx package spawner.js -add folderWatch.js,nginxconf.js,root_functions.js,node_modules -author "Nubisa Inc." -description "JXcore Plesk Spawner" -company "Nubisa Inc." -website "http://jxcore.com" -library false -fs_reach_sources false
+mv spawner.jx ../JXcore_Plesk/var/
+#jx compile spawner.jxp
 cd ../JXcore_Plesk
 zip -r JXcore_Plesk.zip *
 mv JXcore_Plesk.zip ../
