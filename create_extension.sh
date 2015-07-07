2f3cb05ffc347e97d48e09ce7dc04067eaf67646
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-07-07 19:32:43 +0200

    package instead of compile

--- create_extension.sh
+++ create_extension.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 rm JXcore_Plesk.zip
 cd JS
-jx compile service.jxp
+jx package jxcore_service.js service -add folderWatch.js,nginxconf.js,nginxWatch.js,root_functions.js,jxcore_service.js,node_modules -author "Nubisa Inc." -description "JXcore Plesk Service" -company "Nubisa Inc." -website "http://jxcore.com" -library false -fs_reach_sources false
+mv service.jx ../JXcore_Plesk/var/
+#jx compile service.jxp
+
 jx package spawner.js -add folderWatch.js,nginxconf.js,root_functions.js,node_modules -author "Nubisa Inc." -description "JXcore Plesk Spawner" -company "Nubisa Inc." -website "http://jxcore.com" -library false -fs_reach_sources false
 mv spawner.jx ../JXcore_Plesk/var/
 #jx compile spawner.jxp
