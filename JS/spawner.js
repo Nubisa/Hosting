4dfb32e6604297d27c44bf78f705263aee632d6d
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-07-08 16:34:12 +0200

    much better fs watcher/restart manager

--- JS/spawner.js
+++ JS/spawner.js
@@ -9,7 +9,10 @@ var os = require("os");
 
 var whoami = jxcore.utils.cmdSync("whoami").out.toString().trim();
 var isRoot = whoami.toString().trim() === "root";
-var respawned = JSON.stringify(process.argv).indexOf("respawn_id") > -1;
+var _str = JSON.stringify(process.argv);
+// respawn_id is and old arg coming from monitor < v0.3.0.3
+var respawned = _str.indexOf("respawn_id") > -1 || jxcore.monitor.respawned;
+delete _str;
 var exiting = false;
 
 var log = function (str, error) {
@@ -22,6 +25,61 @@ var log = function (str, error) {
     if (fs.existsSync(logPath)) {
         fs.appendFileSync(logPath, str + os.EOL);
     }
+};
+
+var spawner_data_file = process.argv[1] + ".dat";
+var readSpawnerDataFile = function(exitOnError) {
+
+    try {
+        var spawner_data_str = fs.readFileSync(spawner_data_file).toString();
+    } catch (ex) {
+        log("Cannot read spawner data file. Is application disabled in Plesk?", true);
+        if (exitOnError)
+            process.exit(7);
+    }
+
+    var data = null;
+    try {
+        data = JSON.parse(spawner_data_str);
+    } catch(ex){
+        log("Cannot parse spawner data file. " + ex, true);
+        if (exitOnError)
+            process.exit(7);
+    }
+
+    try {
+        if (data) {
+            data.env = parseAppEnv(data.env);
+            return data;
+        }
+    } catch(ex) {
+        log("Cannot parse app environment variables." + ex, true);
+    }
+
+    return null;
+};
+
+// turns string "AA=aaa\nBB=bbb into object
+var parseAppEnv = function(env) {
+    if (!env) {
+        env = {};
+        return;
+    }
+
+    // string expected
+    var lines = env.split("\n");
+    env = {};
+    for(var o in lines) {
+        if (!lines.hasOwnProperty(o))
+            continue;
+
+        var pair = lines[o].split("=");
+        if (pair.length !== 2 || !pair[0].trim() || !pair[1].trim())
+            continue;
+
+        env[pair[0]] = pair[1].trim();
+    }
+    return env;
 };
 
 process.on("uncaughtException", function(err) {
@@ -168,25 +226,13 @@ if (!isRoot || !respawned) {
     // ########  saving nginx conf
     if (options.plesk) {
         try {
-            var spawner_data_file = process.argv[1] + ".dat";
-            if (!fs.existsSync(spawner_data_file)) {
-                log("Cannot find spawner data file. Is application disabled in Plesk Panel?", true);
-                process.exit(7);
-            }
-
-            var spawner_data_str = fs.readFileSync(spawner_data_file).toString();
-            try {
-                spawner_data = JSON.parse(spawner_data_str);
-            } catch(ex){
-                log("Cannot parse spawner data file.", true);
-                process.exit(7);
-            }
+            spawner_data = readSpawnerDataFile(true);
 
             if (spawner_data.disabled) {
                 // exiting to prevent dummy process
                 // however probably this condition will newer be fired, since for disabled apps
                 // we remove spawner fire and spawner data file, so it will end up much sooner
-                log("Application is disabled in Plesk Panel.");
+                log("Application is disabled in Plesk.");
                 process.exit(7);
             }
 
@@ -212,7 +258,7 @@ if (!isRoot || !respawned) {
     }
 
     if (spawner_data && spawner_data.domain)
-        process.title = "jx " + spawner_data.domain;
+        process.title = "jx spawner " + spawner_data.domain;
 
     var child = null;
     // this can be done only by privileged user.
@@ -225,7 +271,18 @@ if (!isRoot || !respawned) {
             if (options.args)
                 args = args.concat(options.args);  //expected array (after options was parsed)
 
-            child = spawn(process.execPath, args, { uid: uid, gid: gid, maxBuffer: 1e7, stdio: [ 'ignore', out, out ], cwd: appDir});
+            var env = spawner_data.env || {};
+//            log("spawner_data " + JSON.stringify(spawner_data, null,4));
+//            log("spawner_data " + JSON.stringify(spawner_data, null,4));
+
+            child = spawn(process.execPath, args, {
+                uid: uid,
+                gid: gid,
+                maxBuffer: 1e7,
+                stdio: [ 'ignore', out, out ],
+                cwd: appDir,
+                env : env
+            });
 
             child.on('error', function (err) {
                 if (err.toString().trim().length) {
@@ -278,7 +335,7 @@ if (!isRoot || !respawned) {
         if (err) {
             log("Did not subscribed (as root) to the monitor: " + txt, true);
         } else {
-            log("Subscribed successfully: " + txt);
+            log("Subscribed successfully (as root): " + txt);
 
             try{
                 runApp();
@@ -288,45 +345,79 @@ if (!isRoot || !respawned) {
                 process.exit();
             }
 
-            var callback = function(event, path) {
+            var watcher = null;
+            var ignored_org = "jxcore_logs/**/*";
 
-                if (pathModule.dirname(path) === pathModule.normalize(logPathDir)) {
+            var watcher_options = {
+                persistent : true,
+                ignoreInitial : true,
+                ignorePermissionErrors : true,
+                followSymlinks : false,
+                interval : 2000,
+                binaryInterval : 2000,
+                // 'fsevents' module was manually deleted from node_modules
+                // (it contained .node files and was not portable)
+                // anyway this module is treated by chokidar as optional
+                useFsEvents : false,
+                usePolling : true, // do not change or you might have "Error: watch ENOSPC"
+                depth : 2
+            };
 
-                    // condition below is not used by jxpanel (only by plesk)
-                    if (options.plesk && path === logClear && out && out != "ignore" ) {
-                        try {
-                            fs.ftruncateSync(out, 0);
-                            log("Log file cleared.");
-                        } catch(ex) {
-                            log("Could not clear the log file: " + ex, true);
-                        }
-                        try {
-                            // removing clearlog.txt
-                            if (fs.existsSync(logClear))
-                                fs.unlinkSync(logClear);
-                        } catch (ex) {
-                            log("Could not remove clearlog file: " + ex, true);
-                        }
+            // callback for internal support (log file/spawner data file)
+            var callbackInternal = function (path, attempt) {
+
+                if (attempt === undefined)
+                    attempt = 0;
+                else
+                    attempt++;
+
+                if (path === logClear && out && out != "ignore") {
+                    try {
+                        fs.ftruncateSync(out, 0);
+                        log("Log file cleared.");
+                    } catch (ex) {
+                        log("Could not clear the log file: " + ex, true);
                     }
-                    // ignore other changes inside log directory
+                    try {
+                        // removing clearlog.txt
+                        if (fs.existsSync(logClear))
+                            fs.unlinkSync(logClear);
+                    } catch (ex) {
+                        log("Could not remove clearlog file: " + ex, true);
+                    }
                     return;
                 }
 
-                if (exiting)
-                    return;
+                if (path === spawner_data_file) {
+                    var data = readSpawnerDataFile(false);
+                    if (data)
+                        watchAppFiles(data);
+                    else {
+                        if (attempt >= 3) {
+                            log("Could not apply changes to Restart Manager.");
+                        } else {
+                            // try again
+                            setTimeout(function() {
+                                callbackInternal(path, attempt);
+                            }, 100);
+                        }
+                    }
+                }
+            };
 
+            // callback for user's apps restart watch
+            var callbackRestart = function(event, path) {
+
+                if (exiting) return;
                 log("File change (" + event + ") detected on " + path.replace(options.home, ""));
-
                 exiting = true;
 
                 var counter = 0;
                 var _inter = setInterval(function(){
                     counter++;
-
                     if(counter>=6 || fs.existsSync(file)){
                         clearInterval(_inter);
-
-                        log("Restarting the application.")
+                        log("Restarting the application.");
                         process.exit(77);
                     }
                 }, 500);
@@ -336,52 +427,111 @@ if (!isRoot || !respawned) {
                 log('Watcher error: ' + error);
             };
 
-            var test = [ "jxcore_logs/**/*",
-                "node_modules",
-                "nope.jx" ];05
+            var watcherReloaded = 0;
 
-            var rg = "(" + test.join("|") + ")";
+            var resolve = function(mask) {
+                mask = mask.trim();
 
-//            for(var o in test) {
-//                test[o] = new RegExp('^' + test[o] + '$');
-//            }
+                if (mask === "MAIN") return file;
 
-            var opts = {
-//                ignored: function(path) {
-//
-//                    var ignore = false;
-////                    for(var o in test) {
-//                        if (rg.test(path)) {
-//                            console.log('IGNORED ' + path);
-//                            return true;
-//                        }
-////                    }
-//                    console.log('NOT IGNORED ' + path);
-//                    return false;
-//                },
-                ignored : rg,
-                persistent : true,
-                ignoreInitial : true,
-                ignorePermissionErrors : true,
-                followSymlinks : false,
-                interval : 2000,
-                binaryInterval : 2000,
-                // 'fsevents' module was manually deleted from node_modules
-                // (it contained .node files and was not portable)
-                // however this module is optional anyway
-                useFsEvents : false,
-                usePolling : false, // do not change or you might have "Error: watch ENOSPC"
-                depth : 2
+                var appRoot = mask.slice(0,2) === "./";
+                if (appRoot)
+                    mask = pathModule.join(appDir, mask.slice(2));
+                else
+                    mask = pathModule.join(appDir, "**", mask);
+
+                return mask;
             };
 
-            var filesToBeWatched = [
-                pathModule.join(appDir, "**", "*.js"),
-                pathModule.join(appDir, "**", "*.jx")
-            ];
+            var watchAppFiles = function(_spawner_data) {
+                var watcher_enabled = true; // default
+                var _ignored = [];
+                var _watched = [];
 
-            chokidar.watch(filesToBeWatched, opts).on('all', callback).on('error', errorCallback);
-            // separately as jxcore_logs is excluded in options
-            chokidar.watch(logClear, { interval : 1000 }).on('all', callback).on('error', errorCallback);
+                // skip if there is no change
+                if (_spawner_data && JSON.stringify(spawner_data.watch) === JSON.stringify(_spawner_data.watch)) {
+//                    log("No change in watcher data.");
+                    return;
+                }
+
+                if (watcherReloaded)
+                    log("Restart Manager is reloading configuration.");
+
+                watcherReloaded++;
+                if (watcherReloaded === Number.MAX_VALUE) watcherReloaded = 1;
+
+                if (!_spawner_data)
+                    _spawner_data = spawner_data;
+
+//                log("watcher before = " + JSON.stringify(_spawner_data.watch).replace(/\n/g, "<br>"));
+                // if there is no spawner_data.watch - the defaults are used
+                if (_spawner_data && _spawner_data.watch) {
+                    var _w = _spawner_data.watch;
+                    if (_w.interval) watcher_options.interval = watcher_options.binaryInterval = _w.interval;
+                    if (_w.depth || _w.depth === 0) watcher_options.depth = _w.depth;
+                    if (_w.enabled != 1) {
+                        watcher_enabled = false;
+                        log("Restart manager is disabled by user.");
+                    }
+
+                    _w.ignore = _w.ignore || "";
+                    if (ignored_org) _w.ignore += "\n" + ignored_org;
+
+                    var _arr = _w.ignore.split("\n");
+                    for(var o = 0, len = _arr.length; o < len; o++) {
+                        var s = _arr[o].trim();
+                        if (s) _ignored.push(resolve(s));
+                    }
+
+                    if (_w.paths) {
+                        var _arr = _w.paths.split("\n");
+                        for(var o = 0, len = _arr.length; o < len; o++) {
+                            var s = _arr[o].trim();
+                            if (s) _watched.push(resolve(s));
+                        }
+                    }
+                }
+
+                if (watcher) {
+                    watcher.close();
+                    watcher = null;
+                }
+
+                if (watcher_enabled) {
+
+                    var filesToBeWatched = _watched;
+
+                    if (!filesToBeWatched.length) filesToBeWatched =[
+                        pathModule.join(appDir, "**", "*.js"),
+                        pathModule.join(appDir, "**", "*.jx")
+                    ];
+
+                    watcher_options.ignored = _ignored;
+
+                    log("Restart manager is active.");
+//                    log("xxx opts = " + JSON.stringify(watcher_options, null, 4).replace(/\n/g, "<br>").replace(/\s\s\s\s/g, "&nbsp;&nbsp;&nbsp;&nbsp;"));
+//                    log("xxx filesToBeWatched  = " + JSON.stringify(filesToBeWatched, null, 4).replace(/\n/g, "<br>").replace(/\s\s\s\s/g, "&nbsp;&nbsp;&nbsp;&nbsp;"));
+                    watcher = chokidar.watch(filesToBeWatched, watcher_options).on('all', callbackRestart).on('error', errorCallback);
+                }
+            };
+
+            if (options.plesk) {
+                var internal_opts = {
+                    interval : 2500,
+                    ignoreInitial : true,
+                    ignorePermissionErrors : true,
+                    useFsEvents : false,
+                    usePolling : true
+                };
+                // this should be watched always, however separately,
+                // since jxcore_logs is excluded (ignored) in options
+                chokidar.watch([ logClear, spawner_data_file ], internal_opts )
+                    .on('add', callbackInternal)
+                    .on('change', callbackInternal)
+                    .on('error', errorCallback);
+            }
+
+            watchAppFiles();
         }
     }, function (delay) {
         log("Subscribing is delayed by " + delay + " ms.");
@@ -390,25 +540,21 @@ if (!isRoot || !respawned) {
     });
 
     var exit = function (code) {
+//        log("xxx exit called");
         try {
             if (child) {
-                //log("!!!! killing child");
-                process.kill(child.pid);
+                var killed = process.kill(child.pid);
+                if (!killed)
+                    log("Spawner could not kill the app when exiting.");
             }
         } catch (ex) {
-            //log("!!!!" + ex);  // 	!!!!ReferenceError: child is not defined
+            log("Spawner could not kill the app when exiting: " + ex);
         }
 
         child = null;
-//if (!code)
         if(!exiting){
             exiting = true;
-            setTimeout(function(){
-                try {
-                    process.exit(77);
-                } catch (ex) {
-                }
-            }, 2000);
+            process.exit(77);
         }
     };
 
@@ -416,4 +562,9 @@ if (!isRoot || !respawned) {
     process.on('SIGBREAK', exit);
     process.on('SIGTERM', exit);
     process.on('SIGINT', exit);
+
+//    log("xxx OnExit attached");
+//
+//    log("respawned ? " + jxcore.monitor.respawned);
+//    log("process.env ? " + JSON.stringify(process.env, null,4));
 }
