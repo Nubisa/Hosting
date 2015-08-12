ff547ba2f04093a1603eddbadf1827b0fa986a64
ktrzeciaknubisa <ktrzeciak@nubisa.com>
2015-08-12 22:31:39 +0200

    remove .DS_Store and compress better

--- create_extension.sh
+++ create_extension.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm JXcore_Plesk.zip
 cd JS
+find ./ -type f -name '.DS_Store' -exec rm {} +
 jx package jxcore_service.js service -add folderWatch.js,nginxconf.js,nginxWatch.js,root_functions.js,jxcore_service.js,node_modules -author "Nubisa Inc." -description "JXcore Plesk Service" -company "Nubisa Inc." -website "http://jxcore.com" -library true -fs_reach_sources false
 mv service.jx ../JXcore_Plesk/var/
 #jx compile service.jxp
@@ -9,6 +10,7 @@ jx package spawner.js -add folderWatch.js,nginxconf.js,root_functions.js,node_mo
 mv spawner.jx ../JXcore_Plesk/var/
 #jx compile spawner.jxp
 cd ../JXcore_Plesk
-zip -r JXcore_Plesk.zip *
+find ./ -type f -name '.DS_Store' -exec rm {} +
+zip -r -9 JXcore_Plesk.zip *
 mv JXcore_Plesk.zip ../
 cd ..
